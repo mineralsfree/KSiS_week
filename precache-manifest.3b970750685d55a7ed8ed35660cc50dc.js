a71ce9fd4f2647464f24161bfe0b492b93e0e75e
Nikita Pilinka <mineralsfree@gmail.com>
2019-03-10 21:43:01 +0300

    Updates

---
+++ precache-manifest.3b970750685d55a7ed8ed35660cc50dc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "466b34818952c80e6631",
+    "url": "/KSiS_week/static/css/main.3da59f45.chunk.css"
+  },
+  {
+    "revision": "466b34818952c80e6631",
+    "url": "/KSiS_week/static/js/main.6a0c14b6.chunk.js"
+  },
+  {
+    "revision": "39e641b892907a06c092",
+    "url": "/KSiS_week/static/js/runtime~main.6b304279.js"
+  },
+  {
+    "revision": "91a88763605cda571068",
+    "url": "/KSiS_week/static/js/2.790a03da.chunk.js"
+  },
+  {
+    "revision": "2d7a7cbbe9dd0d6f85957c9c8ddec788",
+    "url": "/KSiS_week/static/media/Resurs_26.2d7a7cbb.png"
+  },
+  {
+    "revision": "6c6476b1e46598f263fd756596e1011e",
+    "url": "/KSiS_week/static/media/embl_G.6c6476b1.png"
+  },
+  {
+    "revision": "bcbca9dbd892d6894f2d1b81491ca902",
+    "url": "/KSiS_week/static/media/embl_H.bcbca9db.png"
+  },
+  {
+    "revision": "95e45a0206ec7e704b4cfd5b483dc23c",
+    "url": "/KSiS_week/static/media/embl_R.95e45a02.png"
+  },
+  {
+    "revision": "a4bf028fb8995e60784c445449c6ae04",
+    "url": "/KSiS_week/static/media/embl_S.a4bf028f.png"
+  },
+  {
+    "revision": "e3291f33a1ec2425a17ac70be005d67b",
+    "url": "/KSiS_week/static/media/10467.e3291f33.ttf"
+  },
+  {
+    "revision": "2a2699b82cbf6f45f4fe2f838cec4486",
+    "url": "/KSiS_week/static/media/wall.2a2699b8.png"
+  },
+  {
+    "revision": "dc9e3427038d1592bb760d4dc54dcc49",
+    "url": "/KSiS_week/index.html"
+  }
+];
